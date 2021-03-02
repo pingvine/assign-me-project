703fdbf01d98531d19bc7e86d462cdc054564df9
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-02 17:20:35 +0100

    Update urls with course

--- assignme/assignme/urls.py
+++ assignme/assignme/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("assignments/", include("assignments.urls")),
+    path("course/", include("course.urls")),
 ]
---
+++ assignme/course/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("", views.course_index, name="course_index"),
+    path("<int:pk>/", views.course_detail, name="course_detail"),
+    path("<category>/", views.course_category, name="course_category"),
+]
