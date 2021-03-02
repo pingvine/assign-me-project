86822a084f24e58cccd478b4c605075303e0e51f
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-02 17:21:25 +0100

    Init admin settings Post, Comment, Category

--- assignme/course/admin.py
+++ assignme/course/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from course.models import Category, Comment, Post
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Post, PostAdmin)
