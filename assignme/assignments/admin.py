f876f66a4a567cd7681489220ca7bebaea623430
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-02 17:31:03 +0100

    Add assignments to admin

--- assignme/assignments/admin.py
+++ assignme/assignments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from assignments.models import Assignment
 
-# Register your models here.
+
+class AssignmentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Assignment, AssignmentAdmin)
