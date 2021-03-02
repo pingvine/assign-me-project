3d249c3518edc11f1ef595e0f4c75cd3baf9a7eb
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-02 09:16:21 +0100

    Add view for assignment details and index.

--- assignme/assignments/views.py
+++ assignme/assignments/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from assignments.models import Assignment
 
-# Create your views here.
+
+def assignment_index(request):
+    assignments = Assignment.objects.all()
+    context = {
+        'assignments': assignments
+    }
+    return render(request, 'assignment_index.html', context)
+
+
+def assignment_detail(request, pk):
+    assignment = Assignment.objects.get(pk=pk)
+    context = {
+        'assignment': assignment
+    }
+    return render(request, 'project_detail.html', context)
