94a3fc6fc6cc1c596121bedf0f393e0a181b7e64
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-02 21:26:03 +0100

    Refactor project to assignment

--- assignme/assignments/urls.py
+++ assignme/assignments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.assignment_index, name="project_index"),
+    path("", views.assignment_index, name="assignment_index"),
     path("<int:pk>", views.assignment_detail, name="assignment_detail"),
 ]
