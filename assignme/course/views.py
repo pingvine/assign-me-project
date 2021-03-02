edc1b3d6ec1a624a71d91b128351417fed8eb2cc
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-02 17:20:19 +0100

    Add courses views

--- assignme/course/views.py
+++ assignme/course/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from .forms import CommentForm
+from .models import Comment, Post
 
-# Create your views here.
+
+def course_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "course_index.html", context)
+
+
+def course_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "course_category.html", context)
+
+
+def course_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": form,
+    }
+    return render(request, "course_detail.html", context)
