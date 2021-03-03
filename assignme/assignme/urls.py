6c36e9b5420981ac87c5cb38aacd2bf2319ded4f
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-03 10:45:37 +0100

    Add routing index page

--- assignme/assignme/urls.py
+++ assignme/assignme/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("assignments/", include("assignments.urls")),
