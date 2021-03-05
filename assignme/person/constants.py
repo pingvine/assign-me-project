8f7c3a4c9ed242654232565d611575a4da6884cf
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-05 16:19:13 +0100

    Add enums person

---
+++ assignme/person/constants.py
@@ -0,0 +1,15 @@
+TYPE_ASSISTANT = 'Assistant'
+TYPE_COURSE_ASSISTANT = 'Course Assistant'
+TYPE_COURSE_LEADER = 'Course Leader'
+TYPE_EXAMINER = 'Examiner'
+TYPE_ADMINISTRATOR = 'Administrator'
+TPYE_EXTERNAL = 'External'
+
+PERSON_TYPES = (
+    (TYPE_ASSISTANT, 'Assistant'),
+    (TYPE_COURSE_ASSISTANT, 'Course Assistant'),
+    (TYPE_COURSE_LEADER, 'Course Leader'),
+    (TYPE_EXAMINER, 'Examiner'),
+    (TYPE_ADMINISTRATOR, 'Administrator'),
+    (TPYE_EXTERNAL, 'External'),
+)
