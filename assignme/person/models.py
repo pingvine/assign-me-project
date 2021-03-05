3d0b39bc855c821548bb9917fdc7f08585ca7075
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-05 16:37:34 +0100

    Add init person model

--- assignme/person/models.py
+++ assignme/person/models.py
@@ -1,3 +1,31 @@
+import os
+from django.contrib.auth.models import User
 from django.db import models
+from .constants import PERSON_STATUSES
 
-# Create your models here.
+
+class person(models.Model):
+
+    class Meta:
+        verbose_name = 'person'
+        verbose_name_plural = 'persons'
+        ordering = ['first_name', 'last_name']
+
+    first_name = models.CharField('first_name', max_length=20)
+    last_name = models.CharField('last_name', max_length=40)
+    status = models.CharField(choices=PERSON_STATUSES, max_length=50)
+
+    email = models.EmailField('email', blank=True)
+    cellphone = models.CharField('phone', max_length=15, blank=True)
+    image = models.ImageField('picture',
+                              upload_to='/img/profiles',
+                              blank=True,
+                              null=True)
+
+    user = models.OneToOneField(
+        User,
+        blank=True,
+        null=True,
+        verbose_name='user',
+        on_delete=models.CASCADE,
+    )
