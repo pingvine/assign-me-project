22882dd71b0c35f03723c19dba468c233c5b01b1
Jesper Jonsson <jesper_jonsson@hotmail.se>
2021-03-09 10:53:31 +0100

    Add detection of file type by ending

---
+++ assignme/handins/constants.py
@@ -0,0 +1,13 @@
+FILE_TYPE_ADA = 'Ada'
+FILE_TYPE_PYTHON = 'Python'
+FILE_TYPE_CPP = 'C++'
+FILE_TYPE_TXT = 'Plain text'
+
+FILE_TYPE_DEFAULT = 'txt'
+
+FILE_TYPES = (
+    (['adb', 'ads'],                FILE_TYPE_ADA),
+    (['py'],                        FILE_TYPE_PYTHON),
+    (['cpp', 'hpp', 'cc', 'hh'],    FILE_TYPE_CPP),
+    (['plaintext', 'txt', 'text'],  FILE_TYPE_TXT),
+)
--- assignme/handins/models.py
+++ assignme/handins/models.py
@@ -1,14 +1,23 @@
 import os
-from django.db import models
 from datetime import datetime
+from django.db import models
+from .constants import FILE_TYPES, FILE_TYPE_DEFAULT
 
 
 def get_upload_path(instance, filename):
-    return os.path.join(instance.assignment.title,
+    return os.path.join(instance.assignment.title.replace(' ', '-'),
                         instance.holder,
                         datetime.now().strftime("%H-%M-%S"),
                         instance.filename(),
                         )
+
+
+def get_formating_type_from_name(filename):
+    ending = filename.split('.')[-1]
+    for ending_pair in FILE_TYPES:
+        if ending in ending_pair[0]:
+            return ending
+    return FILE_TYPE_DEFAULT
 
 
 class Handin(models.Model):
@@ -24,6 +33,9 @@ class Handin(models.Model):
 
     def __str__(self):
         return '{} - {}'.format(self.holder, self.created_on)
+
+    def get_format_type(self):
+        return get_formating_type_from_name(self.attached_files.name)
 
 
 class Assignment(models.Model):
